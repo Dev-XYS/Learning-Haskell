d8d3efe38ba355a707ce2029992700839be7544b
Dev-XYS <xiaoyushuo0419@live.com>
2020-07-07 18:23:21 +0800

    [code] use `init` to remove the number itself from its factor list
    * To make the code clearer.

--- chapter05-List-comprehensions/6.hs
+++ chapter05-List-comprehensions/6.hs
@@ -2,7 +2,7 @@ factors :: Int -> [Int]
 factors n = [n' | n' <- [1..n], n `mod` n' == 0]
 
 isPerfect :: Int -> Bool
-isPerfect n = n * 2 == sum (factors n)
+isPerfect n = n == sum (init (factors n))
 
 perfect :: Int -> [Int]
 perfect n = [n' | n' <- [1..n], isPerfect n']
